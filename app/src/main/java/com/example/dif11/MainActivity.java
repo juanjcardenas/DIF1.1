f7666bee4c9e211097c8f4f6cee2182cb93fbf40
juanjcardenas <juanchoct123@gmail.com>
2021-08-06 11:38:15 -0500

    commit relacionado con app

--- app/src/main/java/com/example/dif11/MainActivity.java
+++ app/src/main/java/com/example/dif11/MainActivity.java
@@ -4,23 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button r1;
     public TextView rc;
-
+    ToggleButton ojo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         r1 = findViewById(R.id.reg);
         rc = findViewById(R.id.rc);
+        ojo = findViewById(R.id.ojo);
+
+        ojo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    rc.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });
 
         r1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,4 +51,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
--- app/src/main/java/com/example/dif11/Registro.java
+++ app/src/main/java/com/example/dif11/Registro.java
@@ -1,32 +1,98 @@
 package com.example.dif11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Calendar;
 
 public class Registro extends AppCompatActivity implements View.OnClickListener {
 
-    public TextView pis, fn;
+    private TextView nom, ap, gm, pass, passconf, tel, fn, pis;
+    public Button resgistrar;
     private int dia, mes, ano;
+
+    //Variable de los datos que vamos a registrar;
+    private String nombre="";
+    private String apellido="";
+    private String gmail ="";
+    private String clave ="";
+    private String verificacionclave ="";
+    private String telefono="";
+    private String fecha="";
+
+
+    private FirebaseAuth mAuth;//base de datos
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        pis = findViewById(R.id.pis);
+        //ATRAPAMOS LOS VALORES DE LOS ID
+        nom = findViewById(R.id.nom);
+        ap = findViewById(R.id.ap);
+        gm = findViewById(R.id.gmail);
+        pass = findViewById(R.id.pass);
+        passconf = findViewById(R.id.passconf);
+        tel = findViewById(R.id.tel);
         fn = findViewById(R.id.fn);
 
+        //DEFINIMOS --PIS- COMO VARIABLE PARA VOLVER AL LOGIN
+        pis = findViewById(R.id.pis);
 
+        //linea de la instancia
+        mAuth = FirebaseAuth.getInstance();//base de datos necesita una instancia de autenticación de base de fuego
+
+        resgistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nombre = nom.getText().toString();
+                apellido = ap.getText().toString();
+                gmail = gm.getText().toString();
+                clave = pass.getText().toString();
+                verificacionclave = passconf.getText().toString();
+                telefono = tel.getText().toString();
+                fecha = fn.getText().toString();
+
+                if (!nombre.isEmpty() && !apellido.isEmpty() && !gmail.isEmpty() && !clave.isEmpty() && !verificacionclave.isEmpty() && !telefono.isEmpty() && !fecha.isEmpty()){
+
+                    if (clave.length() >= 8 && verificacionclave.length() >= 8){
+                        if(clave.length()==verificacionclave.length()){
+                            registrarUser();
+                        }
+                    }else{
+                        Toast.makeText( Registro.this, "La clave debe tener almenos 8 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText( Registro.this, "Debe completar los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        
         pis.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View registro) {
 
@@ -36,8 +102,41 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
         });
 
         fn.setOnClickListener(this);
+
+    }//aca termina el metodo onCreate
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
     }
 
+    private void updateUI(FirebaseUser currentUser) {
+        Log.i("User:", ""+currentUser);
+    }
+
+    public void createUserWithEmaiñAndpasswored(nombre, apellido){
+
+
+    }
+
+    /** private void registrarUser() {
+        mAuth.createUserWithEmailAndPassword(gmail, clave).addOnCanceledListener(new OnCanceledListener<AuthResult>() {
+            @Override
+            public void onCanceled() {
+                Object task;
+                if (task.isSuccessful()){
+
+                }
+            }
+        });
+    }**/
+
+
+
+    //se invoca los parametros de la fecha en el registro
     @Override
     public void onClick(View v) {
         final Calendar c = Calendar.getInstance();
@@ -51,7 +150,9 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
                 fn.setText(dayOfMonth+"/"+(month+1)+"/"+year);
             }
         }
-        ,dia,mes,ano);
+                ,dia,mes,ano);
         datePickerDialog.show();
     }
-}
+
+
+    }
